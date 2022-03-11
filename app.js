eafbce0adf511d194923a55b1bde03bc37bb8f43
Salko <64722632+sarajlija@users.noreply.github.com>
2022-03-11 13:01:21 +0100

    update minor
    fixed widht on mobile scrin, add js increments number in thevideo  section, minor change at all

--- app.js
+++ app.js
@@ -60,3 +60,26 @@ buttonSearch.addEventListener('click', function (e) {
    
   }
 })
+
+//INCREMENTS NUMBER
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+  counter.innerText = '0'
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute('data-target')
+    const c = +counter.innerText
+
+    const increment = target / 100
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`
+      setTimeout(updateCounter, 50)
+    } else {
+      counter.innerText = target
+    }
+  }
+  console.log(counter)
+  updateCounter()
+})
